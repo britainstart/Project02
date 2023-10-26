312e6beb6b635acacbb9fc4fe617294f7b8bffce
britainstart <bcs5505@psu.edu>
2023-10-26 13:02:39 -0400

    updates

--- src/PolarisChip.js
+++ src/PolarisChip.js
@@ -2,47 +2,120 @@ import { LitElement, html, css } from 'lit';
 
 export class PolarisChip extends LitElement {
   static get properties() {
-    return {
+    return{
       name: { type: String },
       link: { type: String },
-      active: { type: Boolean, reflect: true }
+      active: { type: Boolean, reflect: true },
+      class: { type: String },
     };
   }
 
-  static get styles() {
-    return css`
+    static get styles() {
+      return css`
       :host {
         display: inline-block;
         margin: 0 12px 12px 0;
+        text-transform: uppercase;
+        font-style: italic;
+        padding: 12px 16px 12px 24px;
+        font-weight: bold;
+      }
+      
+              /*
+                    .link:hover,
+                    :host([active]) .link {
+                      background-color: #e4e5e7;
+                      border: 2px solid #e4e5e7;
+                      border-radius: 2px;
+                      color: #005fa9;
+                      cursor: pointer;
+                      text-decoration: underline;
+                    }
+                    */
+                    /*
+                    .link {
+                      text-decoration: none;
+                      padding: 8px 4px;
+                      border: 2px solid #444;
+                      color: #444;
+                      font-size: 16px;
+                    }
+              */
+                    
+      .primaryOutlined {
+        border: 2px solid #005fa9;
+        background-color: #fff;
+        color: #005fa9;
+        padding: 12px 16px 12px 24px;
+        border-radius: 4px;
       }
 
-      .link:focus,
-      .link:hover,
-      :host([active]) .link {
-        background-color: #e4e5e7;
-        border: 2px solid #e4e5e7;
+      .primaryOutlined:hover,
+      :host([active]) .primaryOutlined {
+        background-color: #005fa9;
+        border: 2px solid #005fa9;
+        border-radius: 2px;
+        color: #fff;
+        cursor: pointer;
+        text-decoration: underline;
+        border-radius: 4px;
+      }
+
+      .primaryFilled {
+        background-color: #005fa9;
+        border: 2px solid #005fa9;
+        padding: 12px 16px 12px 24px;
+        color: #fff;
+        border: 2px solid #005fa9;
+        border-radius: 4px;
+      }
+
+      .primaryFilled:hover,
+      :host([active]) .primaryFilled {
+        background-color: #fff;
+        border: 2px solid #005fa9;
         border-radius: 2px;
         color: #005fa9;
         cursor: pointer;
         text-decoration: underline;
-        padding: 12px 10px;
+        border-radius: 4px;
       }
 
-      .link {
-        font-weight: bold;
-        text-decoration: none;
-        padding: 12px 10px;
+      .tinted {
+        background-color: #005fa9;
+        border: 2px solid #fff;
+        padding: 12px 16px 12px 24px;
+        color: #fff;
         border: 2px solid #005fa9;
+        opacity: 0.5;
+        border-radius: 4px;
+      }
+
+      .tinted:hover,
+      :host([active]) .tinted {
+        background-color: #fff;
+        border: 2px solid #005fa9;
+        border-radius: 2px;
         color: #005fa9;
-        font-size: 16px;
+        cursor: pointer;
+        text-decoration: underline;
+        border-radius: 4px;
+      }
+
+      .lightOutlined {
+        border: 2px solid #fff;
+        color: #fff;
+        padding: 12px 16px 12px 24px;
+        border-radius: 4px;
       }
     `;
   }
 
   constructor() {
     super();
-    this.name = 'Football';
+    this.name = '';
     this.link = "https://www.psu.edu/news/research/";
+    this.class = '';
   }
 
   render() {
